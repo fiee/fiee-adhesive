d3a07d791b2cd51d32a44c99c2d878a3fd0f31d8
Henning Hraban Ramm <hraban@fiee.net>
2011-10-07 12:20:40 +0200

    setup ajax calls to use Django’s CSRF tokens

--- adhesive/static/adhesive/js/adhesive.js
+++ adhesive/static/adhesive/js/adhesive.js
@@ -128,6 +128,33 @@ function delete_adhesive(id) {
 }
 
 $(function(){
+  $.ajaxSetup({ cache: false });
+  /* use Django's CSRF tokens in AJAX queries
+   * requires jquery.cookie
+   */ 
+  */
+  $('html').ajaxSend(function(event, xhr, settings) {
+      function getCookie(name) {
+          var cookieValue = null;
+          if (document.cookie && document.cookie != '') {
+              var cookies = document.cookie.split(';');
+              for (var i = 0; i < cookies.length; i++) {
+                  var cookie = jQuery.trim(cookies[i]);
+                  // Does this cookie string begin with the name we want?
+                  if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                      cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                      break;
+                  }
+              }
+          }
+          return cookieValue;
+      }
+      if (!(/^http:.*/.test(settings.url) || /^https:.*/.test(settings.url))) {
+          // Only send the token to relative URLs i.e. locally.
+          xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
+      }
+  });
+
   /* Initialize all notes */
   $('div.adhesive').each(function(index){ init_adhesive(this); });
   
