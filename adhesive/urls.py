c5cc78920a4fb0e5e9c66a42a99a814059ea402c
Henning Hraban Ramm <hraban@fiee.net>
2016-09-20 11:14:17 +0200

    Django 1.10 compat: drop url patterns

--- adhesive/urls.py
+++ adhesive/urls.py
@@ -1,10 +1,10 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 from adhesive.models import Note
 import adhesive.views as sv
 
-urlpatterns = patterns('',
+urlpatterns = [
     url('^(?P<object_id>\w+)/json/$', sv.json_get_for_object, name='notes-get_for_object'),  # makes no sense without object type
     url('^(?P<id>\w+)/json/$', sv.json_get, name='notes-get'),
     url('^(?P<id>\w+)/json/save/$', sv.json_save, name='notes-save'),
     url('^(?P<id>\w+)/json/delete/$', sv.delete, name='notes-delete'),
-)
+]
